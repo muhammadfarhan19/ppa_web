4013531025a86e186d333e71fe739006b5eac815
Muhammad Farhan Hamidie <hamidie.farhan@gmail.com>
2023-08-03 17:32:56 +0700

    Fixing the button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 
 function Contact() {
@@ -8,19 +9,20 @@ function Contact() {
       <section className="hero-section contact-section">
         <h1>gassinn rental di pura-pura adventure</h1>
         <section className="contact-grup">
-          <button className="contact-btn whatsapp">
-            <a href="https://wa.me/6285878982396">chat di sini</a>
-          </button>
-          <button className="contact-btn instagram">
-            <a href="https://www.instagram.com/purapuraadventure/">
+          <Link to="https://wa.me/6285878982396" target="blank">
+            <button className="contact-btn whatsapp">chat di sini</button>
+          </Link>
+          <Link
+            to="https://www.instagram.com/purapuraadventure/"
+            target="blank"
+          >
+            <button className="contact-btn instagram">
               liat instagram kami di sini
-            </a>
-          </button>
-          <button className="contact-btn maps">
-            <a href="https://maps.app.goo.gl/3pdZB8L6Po7pEKHRA" target="blank">
-              lokasi kami
-            </a>
-          </button>
+            </button>
+          </Link>
+          <Link to="https://maps.app.goo.gl/3pdZB8L6Po7pEKHRA" target="blank">
+            <button className="contact-btn maps">lokasi kami</button>
+          </Link>
         </section>
       </section>
     </div>
